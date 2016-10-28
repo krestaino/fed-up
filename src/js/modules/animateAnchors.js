be6334cf3044ae0001110665a7b5867028a39ce6
Kevin Restaino <kevinrestaino@gmail.com>
2016-10-28 00:10:19 -0500

    animateAnchors

--- src/js/modules/animateAnchors.js
+++ src/js/modules/animateAnchors.js
@@ -6,7 +6,7 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 333);
         return false;
       }
     }
